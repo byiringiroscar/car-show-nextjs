d86ade85596d314bac6687fea7f30fc28dff45f2
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-04 13:29:55 +0200

    Add custom button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,11 @@
+'use client';
 import React from 'react'
+import CustomButton from './CustomButton'
 
 const Hero = () => {
+  const handleScroll = () => {
+
+  }
   return (
     <div className='hero'>
       <div className='flex-1 pt-36 padding-x'>
@@ -10,6 +15,11 @@ const Hero = () => {
         <p className='hero__subtitle'>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea inventore placeat, numquam quis ad nostrum.
         </p>
+        <CustomButton
+         title="Explore Cars"
+         containerStyles="bg-primary-blue text-white rounded-full mt-10"
+         handleClick={handleScroll}
+          />
       </div>
     </div>
   )
