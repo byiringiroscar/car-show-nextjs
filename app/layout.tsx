b0da694f746ad2f2e3b9ee09ee90c7c19b745314
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-04 19:44:40 +0200

    wip on nav and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Footer, Navbar } from "@/components";
 
 
 export const metadata: Metadata = {
@@ -15,7 +16,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="relative">
+        <Navbar />
         {children}
+        <Footer />
         </body>
     </html>
   );
