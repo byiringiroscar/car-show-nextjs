1f794d975b8897a0e7418e35d7f6a91291b37151
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-04 13:14:26 +0200

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero'>
+      <div className='flex-1 pt-36 padding-x'>
+        <h1 className='hero__title'>
+          Find, book, or rent a car -- quickly and easily!
+        </h1>
+        <p className='hero__subtitle'>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea inventore placeat, numquam quis ad nostrum.
+        </p>
+      </div>
+    </div>
   )
 }
 
