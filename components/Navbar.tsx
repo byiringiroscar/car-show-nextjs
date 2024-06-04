d2286f5ec44cfd5bba974c1e9617ddc79da5ecaf
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-04 19:59:53 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -21,6 +21,7 @@ const Navbar = () => {
             title='Sign In'
             btnType='button'
             containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]'
+            />
         </nav>
     </header>
   )
