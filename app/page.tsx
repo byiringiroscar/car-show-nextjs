456dd9a3263fa85ebfafea32100917faf32b17d6
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-04 23:24:07 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,15 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
+        </div>
+        <div className='home__filters'>
+
+        </div>
+      </div>
     </main>
   );
 }
