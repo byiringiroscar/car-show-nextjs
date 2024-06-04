25397510cad5227db7d6acda10397b891f490c06
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-04 12:13:29 +0200

    wip on setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="over-flow-hidden">
+      <Hero />
     </main>
   );
 }
