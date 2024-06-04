0e012beeca253b346c25f78751a3edb10c25d2a5
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-04 19:50:09 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+import CustomButton from './CustomButton'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className='w-full absolute z-10'>
+        <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4'>
+            <Link href='/' className='flex justify-center items-center'>
+                <Image
+                src='/logo.svg'
+                alt='Car Hub Logo'
+                width={118}
+                height={18}
+                 />
+            </Link>
+        </nav>
+    </header>
   )
 }
 
