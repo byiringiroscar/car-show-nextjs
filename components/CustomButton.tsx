957e892c92749d860d94e60e7444a324ef64ae8d
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-04 15:33:19 +0200

    Update CustomButton.tsx

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -10,7 +10,7 @@ const CustomButton = ({title, containerStyles, handleClick}: CustomButtonProps) 
     disabled={false}
     type={"button"}
     className={`custom-btn ${containerStyles}`}
-    onClick={() => {}}
+    onClick={handleClick}
     >
         <span className={`flex-1`}>{title}</span>
 
