b1230ae55b85eda8470ebdc17ffc20a4d40c9d73
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-11 10:04:00 +0200

    Add headless ui

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Combobox, Transition } from '@headlessui/react'
 import { SearchManufactureProps } from '@/types'
 
 const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufactureProps) => {
