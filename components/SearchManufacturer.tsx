985d0e944cf6866cadef5f4d3f034d6d0b52a38f
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-11 10:20:25 +0200

    Update SearchManufacturer.tsx

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import { Combobox, Transition } from '@headlessui/react'
 import { SearchManufactureProps } from '@/types'
+import Image from 'next/image'
 
 const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufactureProps) => {
   return (
-    <div>SearchManufacturer</div>
+    <div className='search-manufacturer'>
+        <Combobox>
+            <div className='relative w-full'>
+                <Combobox.Button className='absolute top-[14px]'>
+
+                </Combobox.Button>
+            </div>
+        </Combobox>
+    </div>
   )
 }
 
