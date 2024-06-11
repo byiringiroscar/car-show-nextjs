f972ea7b2ae1368b5a335d78e0a202954d1e1d04
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-11 10:37:30 +0200

    Update SearchManufacturer.tsx

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+'use client';
+import React, {useState, Fragment} from 'react'
 import { Combobox, Transition } from '@headlessui/react'
 import { SearchManufactureProps } from '@/types'
 import Image from 'next/image'
 
 const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufactureProps) => {
+  const [query, setQuery] = useState('')
   return (
     <div className='search-manufacturer'>
         <Combobox>
@@ -17,6 +19,23 @@ const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturePr
                     className='ml-4'
                   />
                 </Combobox.Button>
+                <Combobox.Input
+                  className='search-manufacturer__input'
+                  placeholder='Volkswagen'
+                  displayValue={(manufacturer: string) => manufacturer}
+                  onChange={(e) => setQuery(e.target.value)}
+
+                  />
+
+                  <Transition
+                    as={Fragment}
+                    leave='transition ease-in'
+                    leaveFrom='opacity-100'
+                    leaveTo='opacity-0'
+                    afterLeave={() => setQuery('')}
+                  >
+
+                  </Transition>
             </div>
         </Combobox>
     </div>
