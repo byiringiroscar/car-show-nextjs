b87dd52fa7560672a82675b46cb4f39497aa1e3d
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-12 09:39:50 +0200

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -1,14 +1,4 @@
 
-const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
-const options = {
-  method: 'GET',
-  headers: {
-    'x-rapidapi-key': '3cb604b405mshf995d13abb3c1c2p190d68jsnab455a173d13',
-    'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
-  }
-};
-
-
 
 try {
 	const response = await fetch(url, options);
@@ -16,4 +6,17 @@ try {
 	console.log(result);
 } catch (error) {
 	console.error(error);
+}
+
+
+export async function fetchCars() {
+    const headers = {
+        'x-rapidapi-key': '3cb604b405mshf995d13abb3c1c2p190d68jsnab455a173d13',
+        'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
+      }
+    
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars');
+    
+    
+
 }
