d33ddfc9dff91a6aa66d521d4fd86744cc54b6d2
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-12 11:30:29 +0200

    Update CarCard.tsx

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -29,9 +29,13 @@ const CarCard = ({ car }: CarCardProps) => {
           {make} {model}
         </h2>
       </div>
-      <p>
-        <span>
-          {carRent}
+      <p className='flex mt-6 text-[32px] font-extrabold'>
+        <span className='self-start text-[14px] font-semibold'>
+          $
+        </span>
+        {carRent}
+        <span className='self-end text-[14px] font-medium'>
+          /day
         </span>
       </p>
     </div>
