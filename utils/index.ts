5cb3843f3e46f3fc2c4bc0e523cf24c129fa7696
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-12 09:40:43 +0200

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -1,22 +1,14 @@
-
-
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
-
-
 export async function fetchCars() {
     const headers = {
         'x-rapidapi-key': '3cb604b405mshf995d13abb3c1c2p190d68jsnab455a173d13',
         'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
       }
     
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars');
-    
-    
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars', {
+        headers
+    });
+
+    const result = await response.json();
+    return result;
 
 }
