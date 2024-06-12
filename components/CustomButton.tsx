54fe10835fc654133518e332420fac793247a5c0
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-12 12:02:31 +0200

    Update CustomButton.tsx

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -13,6 +13,11 @@ const CustomButton = ({title, containerStyles, handleClick, btnType, textStyles,
     onClick={handleClick}
     >
         <span className={`flex-1 ${textStyles}`}>{title}</span>
+        {rightIcon && (
+          <div className='relative w-6 h-6'>
+            <Image src={rightIcon} alt='right icon' fill className='object-contain' />
+          </div>
+        )}
 
     </button>
   )
