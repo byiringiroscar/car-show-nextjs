411786db2e7ddc83de6607f74e1397420fba89c3
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-13 21:12:24 +0200

    Update CarDetails.tsx

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -41,7 +41,7 @@ const CarDetails = ({isOpen, closeModal, car}: CarDetailsProps) => {
                         leaveTo='opacity-0 scale-95'
                         >
                             <Dialog.Panel className='relative w-full max-w-lg max-h-[90vh] 
-                             overflow-y-auto transform rounded-2xl text-left shadow-xls transition-all flex flex-col'>
+                             overflow-y-auto transform rounded-2xl bg-white text-left shadow-xls transition-all flex flex-col gap-5'>
                                 <button
                                 type="button"
                                 onClick={closeModal}
@@ -58,7 +58,13 @@ const CarDetails = ({isOpen, closeModal, car}: CarDetailsProps) => {
                                 </button>
                                 <div className='flex-1 flex flex-col gap-3'>
                                     <div className='relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg'>
-                                         <Image src='/hero.png'  alt='model' fill priority className='object-contain' />
+                                         <Image src='/hero.png'  alt='car' fill priority className='object-contain' />
+                                    </div>
+                                    <div className='flex gap-3'>
+                                        <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
+                                            <Image src='/hero.png'  alt='model' fill priority className='object-contain' />
+                                        </div>
+
                                     </div>
                                 </div>
                             </Dialog.Panel>
