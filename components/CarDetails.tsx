eef846df7450543e835fbfcccae4f9bdffc79cc4
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-14 16:28:11 +0200

    Update CarDetails.tsx

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -59,17 +59,17 @@ const CarDetails = ({isOpen, closeModal, car}: CarDetailsProps) => {
                                 </button>
                                 <div className='flex-1 flex flex-col gap-3'>
                                     <div className='relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg'>
-                                         <Image src={generateCarImageUrl(car, 'angle')}  alt='car' fill priority className='object-contain' />
+                                         <Image src={generateCarImageUrl(car)}  alt='car' fill priority className='object-contain' />
                                     </div>
                                     <div className='flex gap-3'>
                                         <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
-                                            <Image src={generateCarImageUrl(car, 'angle')}  alt='model' fill priority className='object-contain' />
+                                            <Image src={generateCarImageUrl(car, '29')}  alt='model' fill priority className='object-contain' />
                                         </div>
                                         <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
-                                            <Image src={generateCarImageUrl(car, 'angle')}  alt='model' fill priority className='object-contain' />
+                                            <Image src={generateCarImageUrl(car, '33')}  alt='model' fill priority className='object-contain' />
                                         </div>
                                         <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
-                                            <Image src={generateCarImageUrl(car, 'angle')}  alt='model' fill priority className='object-contain' />
+                                            <Image src={generateCarImageUrl(car, '13')}  alt='model' fill priority className='object-contain' />
                                         </div>
 
                                     </div>
