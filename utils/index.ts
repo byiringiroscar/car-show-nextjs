80ec2c99f983fd5f6d743059e492beab1b6193aa
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-14 11:45:12 +0200

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -32,7 +32,17 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   };
 
 
-
   export const generateCarImageUrl = (car: CarProps, angle?: string) => {
-    
-  }
+    const url = new URL("https://cdn.imagin.studio/getimage");
+    const { make, model, year } = car;
+  
+    url.searchParams.append('customer','hrjavascript-mastery' || '');
+    url.searchParams.append('make', make);
+    url.searchParams.append('modelFamily', model.split(" ")[0]);
+    url.searchParams.append('zoomType', 'fullscreen');
+    url.searchParams.append('modelYear', `${year}`);
+    // url.searchParams.append('zoomLevel', zoomLevel);
+    url.searchParams.append('angle', `${angle}`);
+  
+    return `${url}`;
+  } 
