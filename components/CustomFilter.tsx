c5dc3f8e45a34b21f4421f85edb4755457dde09d
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-16 17:31:07 +0200

    Update CustomFilter.tsx

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -24,6 +24,28 @@ const CustomFilter = ({title, options}: CustomFilterProps) => {
                 className='ml-4 object-contain'
               />
           </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave='transition ease-in duration-100'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+          >
+            <Listbox.Options className='custom-filter__options'>
+              {options.map((option) => (
+                <Listbox.Option key={option.title} value={option}>
+                  {({ active }) => (
+                    <div
+                      className={`${
+                        active ? 'text-white bg-blue-600' : 'text-gray-900'
+                      } cursor-pointer select-none relative py-2 pl-10 pr-4`}
+                    >
+                      <span className='block truncate'>{option.title}</span>
+                    </div>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
         </div>
       </Listbox>
     </div>
