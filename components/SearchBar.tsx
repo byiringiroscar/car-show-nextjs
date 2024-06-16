9639f07b9083f60ad48ecfc4f5cf6af2f927f2e0
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-16 16:50:25 +0200

    Update SearchBar.tsx

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -51,7 +51,7 @@ const SearchBar = () => {
 
     const newPathname = `${window.location.pathname}?${searchParams.toString()}`
 
-    router.push(newPathname)
+    router.push(newPathname, {scroll: false})
   }
   return (
     <form action="" className='searchbar' onSubmit={handleSearch}>
