711bcb6075cfea46843cd8845eafe80057013b5e
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-16 21:07:54 +0200

    Update ShowMore.tsx

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -5,16 +5,12 @@ import { ShowMoreProps } from '@/types'
 import CustomButton from './CustomButton'
 import { updateSearchParams } from '@/utils'
 
-const ShowMore = ({ pageNumber, isNext}: ShowMoreProps) => {
+const ShowMore = ({ pageNumber, isNext, setLimit}: ShowMoreProps) => {
   const router = useRouter();
 
   const handleNavigation = () => {
     const newLimit = (pageNumber + 1) * 10;
-    const newPathName = updateSearchParams('limit', `${newLimit}`);
-
-    router.push(newPathName, {
-      scroll: false
-    })
+    setLimit(newLimit);
   }
   return (
     <div className='w-full flex-center gap-5 mt-10'>
