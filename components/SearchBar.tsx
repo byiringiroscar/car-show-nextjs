58505ac6b64f0bb101e27aa59a9215219614eaf9
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-16 16:30:59 +0200

    Update SearchBar.tsx

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,7 +2,7 @@
 import React, { useActionState, useState, useEffect } from 'react'
 import SearchManufacturer from './SearchManufacturer'
 import Image from 'next/image'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 
 
 const SearchButton = ({otherClasses}: { otherClasses: string}) => {
