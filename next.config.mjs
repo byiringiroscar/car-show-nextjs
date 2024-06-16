c71c41e62254936f5e5fd4ab8334596f855fa72c
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-16 21:12:32 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,11 @@
 const nextConfig = {
     images: {
         domains: ['cdn.imagin.studio', 'source.unsplash.com'],
+    },
+    typescript: {
+        ignoreBuildErrors: true,
     }
+
 };
 
 export default nextConfig;
