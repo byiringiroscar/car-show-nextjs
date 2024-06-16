feb206f2d310fc41114be2b5e3868a9c394534fc
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-16 17:20:33 +0200

    wip on custom filter

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
 import Image from "next/image";
 import { fetchCars } from "@/utils";
-import { manufacturers } from "@/constants";
+import { fuels, manufacturers, yearsOfProduction } from "@/constants";
+import { FilterProps } from "@/types";
 
 export default async function Home({ searchParams }) {
   const allCars = await fetchCars({
@@ -23,8 +24,8 @@ export default async function Home({ searchParams }) {
         <div className='home__filters'>
           <SearchBar />
           <div className='home__filter-container'>
-              <CustomFilter />
-              <CustomFilter />
+              <CustomFilter title='fuel' options={fuels} />
+              <CustomFilter title='year' options={yearsOfProduction} />
             </div>
         </div>
 
--- types/index.ts
+++ types/index.ts
@@ -39,3 +39,14 @@ export interface FilterProps {
     limit: number;
     model: string;
 }
+
+export interface OptionProps {
+    title: string;
+    value: string;
+}
+
+
+export interface CustomFilterProps {
+    title: string;
+    options: OptionProps[];
+}
