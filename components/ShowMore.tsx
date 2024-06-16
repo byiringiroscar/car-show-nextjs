4790450e2e503b05dfae15ed3dfbe244220acf9c
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-16 19:32:50 +0200

    Update ShowMore.tsx

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -3,11 +3,19 @@ import React from 'react'
 import { useRouter } from 'next/navigation'
 import { ShowMoreProps } from '@/types'
 import CustomButton from './CustomButton'
+import { updateSearchParams } from '@/utils'
 
 const ShowMore = ({ pageNumber, isNext}: ShowMoreProps) => {
   const router = useRouter();
 
-  const handleNavigation = () => {}
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10;
+    const newPathName = updateSearchParams('limit', `${newLimit}`);
+
+    router.push(newPathName, {
+      scroll: false
+    })
+  }
   return (
     <div className='w-full flex-center gap-5 mt-10'>
         {!isNext && (
