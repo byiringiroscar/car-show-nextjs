60dd16d078a1dcf315f27f184d2f3e6d38bbb0ca
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-16 17:21:14 +0200

    Update CustomFilter.tsx

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -3,8 +3,9 @@ import React, {useState, Fragment} from 'react'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import { Listbox, Transition } from '@headlessui/react'
+import { CustomButtonProps, CustomFilterProps } from '@/types'
 
-const CustomFilter = ({title, options}) => {
+const CustomFilter = ({title, options}: CustomFilterProps) => {
   return (
     <div>CustomFilter</div>
   )
