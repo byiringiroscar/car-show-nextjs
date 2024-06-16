af1489dddc468bfa26bb393c4ebe2a976ff50063
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-16 18:57:06 +0200

    Add custom filter

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -4,14 +4,30 @@ import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import { Listbox, Transition } from '@headlessui/react'
 import { CustomButtonProps, CustomFilterProps } from '@/types'
+import { updateSearchParams } from '@/utils'
 
 const CustomFilter = ({title, options}: CustomFilterProps) => {
+  const router = useRouter()
   const [selected, setSelected] = useState(options[0])
+
+
+  const handleUpdateParams = (e: {title: string, value: string }) => {
+    const newPathName =  updateSearchParams(title, e.value.toLowerCase());
+    router.push(newPathName, {scroll: false})
+
+  }
+
+
   return (
     <div className='w-fit'>
       <Listbox
         value={selected}
-        onChange={(e) => setSelected(e)}
+        onChange={(e) => {
+            setSelected(e)
+            handleUpdateParams(e)
+        }
+           
+          }
       >
         <div className='relative w-fit z-10'>
           <Listbox.Button className='custom-filter__btn'>
--- utils/index.ts
+++ utils/index.ts
@@ -49,3 +49,11 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   
     return `${url}`;
   } 
+
+
+export const updateSearchParams = (type: string, value: string) => {
+  const searchParams = new URLSearchParams(window.location.search);
+  searchParams.set(type, value);
+  const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+  return newPathname
+}
