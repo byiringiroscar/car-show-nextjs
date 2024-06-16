c4f0068fb06c69e2ba1e272a6a58d4d051e06f85
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-16 19:26:21 +0200

    Update ShowMore.tsx

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -2,10 +2,24 @@
 import React from 'react'
 import { useRouter } from 'next/navigation'
 import { ShowMoreProps } from '@/types'
+import CustomButton from './CustomButton'
 
 const ShowMore = ({ pageNumber, isNext}: ShowMoreProps) => {
+  const router = useRouter();
+
+  const handleNavigation = () => {}
   return (
-    <div>ShowMore</div>
+    <div className='w-full flex-center gap-5 mt-10'>
+        {!isNext && (
+            <CustomButton
+                title='Show More'
+                btnType='button'
+                handleClick={handleNavigation}
+                containerStyles='bg-primary-blue text-white rounded-full'
+            />
+
+        )}
+    </div>
   )
 }
 
