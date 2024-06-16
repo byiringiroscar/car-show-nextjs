ab0ea72b9680de718f2997dd27e9f40967e040f7
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-06-16 17:29:48 +0200

    Update CustomFilter.tsx

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -9,10 +9,20 @@ const CustomFilter = ({title, options}: CustomFilterProps) => {
   const [selected, setSelected] = useState(options[0])
   return (
     <div className='w-fit'>
-      <Listbox>
+      <Listbox
+        value={selected}
+        onChange={(e) => setSelected(e)}
+      >
         <div className='relative w-fit z-10'>
           <Listbox.Button className='custom-filter__btn'>
-              <span>Filter</span>
+              <span className='block truncate'>{selected.title}</span>
+              <Image
+                src='/chevron-up-down.svg'
+                alt='chevron up down'
+                width={20}
+                height={20}
+                className='ml-4 object-contain'
+              />
           </Listbox.Button>
         </div>
       </Listbox>
